86ad0af7912c5bad5d40023e72f6f40001076491
fryadkazm <fryadkazm94@gmail.com>
2025-12-17 01:29:07 -0800

    update index and style

---
+++ logic/index.js
@@ -0,0 +1,3 @@
+"use strict";
+document.querySelector(".copyright").textContent = new Date().getFullYear();
+// alternative text for the rock image: A closed fist representing rock in the rock paper scissors game
